4fb727112e0aa1a0698c9c1ba16eef2b75d5cf5f
Matthew Grossman <grossmanmatt@yahoo.com>
2017-02-08 07:28:12 -0500

    first commit

---
+++ vacation_adventure.rb
@@ -0,0 +1,12 @@
+
+random_number = rand(3) + 1
+
+puts "The random number is  #{random_number}"
+
+if random_number == 1
+  puts  "Go sailing"
+elsif random_number == 2
+  puts  "Go to an animal reservation"
+else
+  puts "Go jet skiing"
+end
